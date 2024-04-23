f872eb4ccf40608e7e6c3d96a861b4d8c7baa48a
rsotomay <138070004+rsotomay@users.noreply.github.com>
2024-04-23 16:53:38 -0500

    Adding token and test

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -6,5 +6,15 @@ import "hardhat/console.sol";
 contract Token {
     string public name;
     string public symbol;
-    uint public totalSupply;
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _totalSupply) {
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _totalSupply * (10**decimals);
+    }
 }
