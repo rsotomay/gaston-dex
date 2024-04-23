dcd155d8440462882c44223a558cebea638ee3ad
rsotomay <138070004+rsotomay@users.noreply.github.com>
2024-04-23 13:06:41 -0500

    Smoke test

---
+++ contracts/Token.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED 
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Token {
+    string public name;
+    string public symbol;
+    uint public totalSupply;
+}
---
+++ scripts/1_deploy.js
@@ -0,0 +1,16 @@
+async function main() {
+  //Ferch contract to deploy
+  const Token = await ethers.getContractFactory("Token");
+
+  //Deploy contract
+  const token = await Token.deploy();
+  await token.deployed();
+  console.log(`Token deployed to ${token.address}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
